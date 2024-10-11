8de7a009d33f8de86ef12724b9528de7a6456d5f
Dante <r0841336@student.thomasmore.be>
2024-10-11 14:33:21 +0200

    test

---
+++ controllers/api/v1/messages.js
@@ -0,0 +1,38 @@
+const Message = require("../../../models/api/v1/Message");
+
+const create = (req, res) => {
+
+    const text = req.body.message.text;
+    const user = req.body.message.user;
+
+    const m = new Message({ user: user, text: text });
+    m.save().then(() => {
+
+        res.json({
+            status: "succes", 
+            data: {
+                message: m,
+            },
+        });
+
+    });
+
+};
+
+const index = async (req, res) => {
+
+    const messages = await Message.find();
+    res.json({
+        status: "succes",
+        data:{ 
+            messages: messages,
+        },
+    });
+
+};
+
+
+module.exports = {
+    create,
+    index,
+};
